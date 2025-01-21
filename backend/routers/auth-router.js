514ef98a16759d420a3f3a407d78624947e9a9af
yokins <yokins.shi@icloud.com>
2025-01-21 15:36:08 +0800

    化纯

--- backend/routers/auth-router.js
+++
@@ -1,8 +0,0 @@
-import Router from "koa-router";
-import AuthController from "../controllers/auth-controller.js";
-
-const router = new Router();
-
-router.post("/login", AuthController.login);
-
-export default router;
