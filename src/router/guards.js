cd76490fbb8d584e0a770c3d4542dce6de154b1a
yokins <yokins.shi@icloud.com>
2025-01-24 14:36:30 +0800

    添加路由守卫

---
+++ src/router/guards.js
@@ -0,0 +1,48 @@
+import "nprogress/nprogress.css";
+import NProgress from "nprogress";
+import { useGlobalStore } from "@/stores/global";
+
+NProgress.configure({ showSpinner: false });
+
+// 按照想要加载的顺序来，从开始到结束
+export default {
+    before: {
+        /**
+         * @description: 开始加载进度条
+         * @return {*}
+         */
+        nProgressStart() {
+            NProgress.start();
+        },
+        /**
+         * @description: 检查是否需要登录
+         * @return {*}
+         */
+        checkLogin(to, from, next) {
+            const globalStore = useGlobalStore();
+
+            if (globalStore.currentUser) {
+                if (to.name == "root" || to.name == "auth_login") {
+                    next({ name: "auth_login" });
+                } else {
+                    next();
+                }
+            } else {
+                if (to.meta?.skipAuth) {
+                    next();
+                } else {
+                    next({ name: "auth_login" });
+                }
+            }
+        }
+    },
+    after: {
+        /**
+         * @description: 结束进度条
+         * @return {*}
+         */
+        nProgressEnd() {
+            NProgress.done();
+        }
+    }
+};
