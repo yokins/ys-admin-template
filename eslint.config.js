2ab2906770388fb9d09a3e297b6eb8e0cb1eee1d
yokins <yokins.shi@icloud.com>
2025-06-17 10:32:08 +0800

    初始化

---
+++ eslint.config.js
@@ -0,0 +1,53 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginVue from "eslint-plugin-vue";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+import pluginPrettier from "eslint-plugin-prettier";
+import autoImportConfig from "./.eslintrc-auto-import.json";
+
+export default defineConfig([
+    {
+        files: ["**/*.{js,mjs,cjs,vue}"],
+        plugins: { js },
+        extends: ["js/recommended"]
+    },
+    {
+        files: ["**/*.{js,mjs,cjs,vue}"],
+        languageOptions: { globals: { ...globals.browser, ...autoImportConfig.globals } }
+    },
+    pluginVue.configs["flat/essential"],
+    pluginPrettier.configs.recommended,
+    {
+        files: ["**/*.json"],
+        plugins: { json },
+        language: "json/json",
+        extends: ["json/recommended"]
+    },
+    {
+        files: ["**/*.jsonc"],
+        plugins: { json },
+        language: "json/jsonc",
+        extends: ["json/recommended"]
+    },
+    {
+        files: ["**/*.json5"],
+        plugins: { json },
+        language: "json/json5",
+        extends: ["json/recommended"]
+    },
+    {
+        files: ["**/*.md"],
+        plugins: { markdown },
+        language: "markdown/commonmark",
+        extends: ["markdown/recommended"]
+    },
+    {
+        files: ["**/*.css"],
+        plugins: { css },
+        language: "css/css",
+        extends: ["css/recommended"]
+    }
+]);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import eslintPlugin from "vite-plugin-eslint";
+import AutoImport from "unplugin-auto-import/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+    plugins: [
+        vue(),
+        eslintPlugin(),
+        AutoImport({
+            imports: [
+                "vue",
+                "vue-router",
+                "vue-i18n",
+                {
+                    "naive-ui": ["useDialog", "useMessage", "useNotification", "useLoadingBar"]
+                }
+            ],
+            eslintrc: {
+                enabled: true,
+                filepath: "./.eslintrc-auto-import.json",
+                globalsPropValue: true
+            }
+        })
+    ]
+});
